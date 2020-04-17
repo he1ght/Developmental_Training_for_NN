45eb93389f12595f4dabe52c74cc079749230ced
he1ght <dkcla0223@gmail.com>
2020-04-17 23:14:43 +0900

    blank exception

--- remove_blank.py
+++ remove_blank.py
@@ -2,4 +2,5 @@ import sys
 
 import prune_data
 
-prune_data.remove_blank(sys.argv[1], sys.argv[2])
+# prune_data.remove_blank(sys.argv[1], sys.argv[2])
+prune_data.remove_blank_sync(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
