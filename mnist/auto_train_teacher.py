d98da217f0fa7cdaea19a724a155700a91511ef9
HeeSeung Jung <heesng.jung@gmail.com>
2020-07-10 15:36:48 +0900

    Auto train teacher model

--- mnist/auto_train_teacher.py
+++ mnist/auto_train_teacher.py
@@ -5,7 +5,7 @@ from glob import glob
 
 data_size_list = ['100', '1000', '3000', '10000']
 data_seed_list = ['1', '2', '3']
-dropout_lit = ['0.01', '0.1', '0.3', '0.5', '0.7', '0.9', '0.9', '0.99']
+dropout_list = ['0.01', '0.1', '0.3', '0.5', '0.7', '0.9', '0.9', '0.99']
 # Penalty
 # Batch Norm
 
@@ -15,9 +15,9 @@ base_pred_path = './predict/'
 
 fl = 'train_teacher_mnist.py'
 for d_size in data_size_list:
-    for do in dropout_lit:
+    for do in dropout_list:
         for d_seed in data_seed_list:
             data_name = d_size + '_s' + d_seed
-            save_name = data_name + '-' + dropout_lit + 'do'
+            save_name = data_name + '-' + do + 'do'
             subprocess.call(['python', fl, '--epochs 50 --batch-size 128 --lr 0.01 --hidden 500 --tensorboard',
                              '--seed', d_seed, '--data', data_name, '--dropout', do, '--save', save_name])
